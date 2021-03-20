5d3c269754981cb536fc31c3c52d447a11b25aca
Alexandr2105 <5030553@gmail.com>
2021-03-20 14:21:47 +0300

    HomeWork

--- src/HW_19032021/Computer.java
+++ src/HW_19032021/Computer.java
@@ -13,31 +13,48 @@ public class Computer {
         System.out.println(hardDiskSize);
     }
 
+    private static int countOn = 0;
+    private static int countOff = 0;
+
     public void turnOnComputer() {
+        if (countOn >= 1) {
+            System.out.println("Компьютер уже включён и работает");
+            System.exit(0);
+        }
         System.out.println("Ввести значение 0 либо 1");
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = (int) (Math.random() * 2);
-        System.out.println(b);
-        if (a < 0 || a > 2) {
+        if (a < 0 || a >= 2) {
             System.out.println("Ошибка не верное значение");
         }
         if (a != b) {
             System.out.println("Компьютеру конец");
             System.exit(0);
+        } else {
+            System.out.println("Компьютер включён и работает");
+            countOn++;
         }
     }
 
     public void turnOffComputer() {
+        if (countOff >= 1) {
+            System.out.println("Компьютер уже выключен");
+            System.exit(0);
+        }
         System.out.println("Ввести значение 0 либо 1");
         Scanner scanner = new Scanner(System.in);
         int c = scanner.nextInt();
         int d = (int) (Math.random() * 2);
-        if (c < 0 || c > 2) {
+        if (c < 0 || c >= 2) {
             System.out.println("Ошибка не верное значение");
         }
         if (c != d) {
             System.out.println("Компьютеру конец");
+            System.exit(0);
+        } else {
+            System.out.println("Компьютер выключен");
+            countOff++;
         }
     }
 
